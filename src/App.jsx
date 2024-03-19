d8c7a4080dba29b192488fbd1a3f58ce138f56be
diego <dgcuenca@gmail.com>
2024-03-18 20:10:36 -0500

    Handle key id

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { React, useState } from 'react'
 import './App.css'
 import Die from './components/Die.jsx'
+import { nanoid } from 'nanoid'
 
 function App() {
   const [dice, setDice] = useState(() => getDiceValues())
@@ -8,18 +9,29 @@ function App() {
   function getDiceValues() {
     let diceValues = [];
     for (let i = 0; i <= 9; i++) {
-      diceValues.push(Math.ceil(Math.random() * 6)); //from 1 to 6
+      diceValues.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+      });
     }
     return diceValues;
   }
 
-  const diceComponents = dice.map(die => <Die value={die} />);
+  function rollDice() {
+    setDice(getDiceValues())
+  }
+
+  const diceComponents = dice.map(die => (
+    <Die key={die.id} value={die.value} isHeld={die.isHeld} />
+  ));
 
   return (
     <main>
       <div className="dice-container">
         {diceComponents}
       </div>
+      <button className="roll-dice" onClick={rollDice}>Roll</button>
     </main>
   )
 }
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import '../App.css'
 
 function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white"
+  }
+
   return (
-    <div className="die-face">
-      <button className="die-num">{props.value}</button>
+    <div className="die-face" style={styles}>
+      <button className="die-num" style={styles}>
+        {props.value}
+      </button>
     </div>
   )
 }
